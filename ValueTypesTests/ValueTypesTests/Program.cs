577d692c408079b5fac798c9ac643b2d75cb2c70
Vadym <Vadymshmorgun23@gmail.com>
2015-12-06 12:55:00 +0200

    Add ValueTypesTests to repository

--- ValueTypesTests/ValueTypesTests/Program.cs
+++ ValueTypesTests/ValueTypesTests/Program.cs
@@ -10,31 +10,31 @@ namespace ValueTypesTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(typeof(sbyte).FullName  + "The size of sbyte is {0}.", sizeof(sbyte));
+            Console.WriteLine(typeof(sbyte).FullName  + " " + "The size of sbyte is {0}.", sizeof(sbyte));
             Console.WriteLine();
-            Console.WriteLine(typeof(bool).FullName + "The size of bool is {0}.", sizeof(bool));
+            Console.WriteLine(typeof(bool).FullName + " " + "The size of bool is {0}.", sizeof(bool));
             Console.WriteLine();
-            Console.WriteLine(typeof(byte).FullName + "The size of byte is {0}.", sizeof(byte));
+            Console.WriteLine(typeof(byte).FullName + " " + "The size of byte is {0}.", sizeof(byte));
             Console.WriteLine();
-            Console.WriteLine(typeof(char).FullName + "The size of char is {0}.", sizeof(char));
+            Console.WriteLine(typeof(char).FullName + " " + "The size of char is {0}.", sizeof(char));
             Console.WriteLine();
-            Console.WriteLine(typeof(decimal).FullName + "The size of decimal is {0}.", sizeof(decimal));
+            Console.WriteLine(typeof(decimal).FullName + " " + "The size of decimal is {0}.", sizeof(decimal));
             Console.WriteLine();
-            Console.WriteLine(typeof(double).FullName + "The size of double is {0}.", sizeof(double));
+            Console.WriteLine(typeof(double).FullName + " " + "The size of double is {0}.", sizeof(double));
             Console.WriteLine();
-            Console.WriteLine(typeof(float).FullName + "The size of float is {0}.", sizeof(float));
+            Console.WriteLine(typeof(float).FullName + " " + "The size of float is {0}.", sizeof(float));
             Console.WriteLine();
-            Console.WriteLine(typeof(int).FullName + "The size of int is {0}.", sizeof(int));
+            Console.WriteLine(typeof(int).FullName + " " + "The size of int is {0}.", sizeof(int));
             Console.WriteLine();
-            Console.WriteLine(typeof(long).FullName + "The size of long is {0}.", sizeof(long));
+            Console.WriteLine(typeof(long).FullName + " " + "The size of long is {0}.", sizeof(long));
             Console.WriteLine();
-            Console.WriteLine(typeof(short).FullName + "The size of short is {0}.", sizeof(short));
+            Console.WriteLine(typeof(short).FullName + " " + "The size of short is {0}.", sizeof(short));
             Console.WriteLine();
-            Console.WriteLine(typeof(uint).FullName + "The size of uint is {0}.", sizeof(uint));
+            Console.WriteLine(typeof(uint).FullName + " " + "The size of uint is {0}.", sizeof(uint));
             Console.WriteLine();
-            Console.WriteLine(typeof(ulong).FullName + "The size of ulong is {0}.", sizeof(ulong));
+            Console.WriteLine(typeof(ulong).FullName + " " + "The size of ulong is {0}.", sizeof(ulong));
             Console.WriteLine();
-            Console.WriteLine(typeof(ushort).FullName + "The size of ushort is {0}.", sizeof(ushort));
+            Console.WriteLine(typeof(ushort).FullName + " " + "The size of ushort is {0}.", sizeof(ushort));
             Console.WriteLine();
 
             Console.Read();
