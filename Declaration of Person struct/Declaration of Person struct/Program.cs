cd784b19f425cba41fddcb027f43a76a3796d916
Vadym <Vadymshmorgun23@gmail.com>
2015-12-12 11:47:36 +0200

    Declaration of Person struct

---
+++ Declaration of Person struct/Declaration of Person struct/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Declaration_of_Person_struct
+{
+    class Person
+    {
+        public string personName;
+        public string personSurname;
+        public int personBirthday;
+        public string personMarriagestatus;
+        public string personSex;
+
+        public Person(string name, string surname, int birthday, string marriagestatus, string sex)
+        {
+            personName = name;
+            personSurname = surname;
+            personBirthday = birthday;
+            personMarriagestatus = marriagestatus;
+            personSex = sex;
+        }
+        public Person() { }
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Surname: {1}, Birthday: {2}, Marriagestatus: {3}, Sex: {4}", personName, personSurname, personBirthday, personMarriagestatus, personSex);
+
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Person Mel = new Person("Nikki", "Smith", 1990, "Single", "Male");
+
+            Mel.Display();
+                   
+            Console.Read();
+        }
+    }
+}
--- PointParser/PointParser/Program.cs
+++ PointParser/PointParser/Program.cs
@@ -8,8 +8,28 @@ namespace PointParser
 {
     class Program
     {
-        static void Main(string[] args)
+        struct Point
         {
+            public int X;
+            public int Y;
+
+
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+           
+            static void Main(string[] args)
+            {
+                Console.Write("Please, enter coordinates: ");
+                int result = Int32.Parse(Console.ReadLine());
+                
+
+                Console.WriteLine("You entered the following coordinates: ");
+
+                Console.Read();
+            }
         }
     }
 }
