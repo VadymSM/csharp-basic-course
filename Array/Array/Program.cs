6a9fbb38bead04736ffb38d99925a145cf20ca64
Vadym <Vadymshmorgun23@gmail.com>
2015-12-21 23:18:56 +0200

    Updated

---
+++ Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] strings = new int[1000];
+            for (int i = 1; i < strings.Length; i++)
+            {
+                i = i + 9;
+                Console.WriteLine(i);
+            }
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ ArrayNameSurname/ArrayNameSurname/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayNameSurname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] name = new string[] { "Mike", "Ira", "Nick" };
+                string[] surname = new string[] { "Smith", "Kiten", "Mitan" };
+
+                for (int i = 0; i < name.Length; ++i)
+                {
+                    for (int j = 0; j < surname.Length; ++j)
+                    {
+                        Console.WriteLine(name[i] + " " + surname[j]);
+                        
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
