f2ba1abacbc1ad6a660e805d1190d5001049a2f0
Vadym <Vadymshmorgun23@gmail.com>
2015-12-27 11:38:08 +0200

    Add

---
+++ EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key;
+            int[] numbers = new int[100];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] + i;
+                if (i % 2 == 0)
+                {
+                    if (i == 10)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(i);
+                }              
+            }
+            Console.WriteLine("Do you want to continue (Y/N)?: ");
+            key = Console.ReadLine();
+            
+            Console.ReadLine();
+        }
+    }
+}
