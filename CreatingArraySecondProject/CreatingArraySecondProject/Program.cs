20428b6dfcc2060c66a5d5f42f54a17d116334e1
Vadym <Vadymshmorgun23@gmail.com>
2016-03-01 23:01:29 +0200

    add

---
+++ CreatingArraySecondProject/CreatingArraySecondProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingArraySecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] array = new int[size];
+                    
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter element: ");
+                int number = int.Parse(Console.ReadLine());
+                array[i] = number;
+                            
+            }
+
+            for(int j = 0; j < array.Length - 1; j+=2)
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                
+            }
+
+            for(int k = 0; k < array.Length; k++)
+            {
+                Console.WriteLine(array[k] + " ");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
