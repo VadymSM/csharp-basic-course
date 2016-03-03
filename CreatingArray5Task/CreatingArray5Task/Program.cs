4f5bfdba00d95ac3f13ed0306e130b2831c2e006
Vadym <Vadymshmorgun23@gmail.com>
2016-03-03 12:22:07 +0200

    add

---
+++ CreatingArray5Task/CreatingArray5Task/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingArray5Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] array = new int[size];
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter element: ");
+                int number = int.Parse(Console.ReadLine());
+                array[i] = number;
+            }
+
+            int Minimum = 0;
+            int Maximum = 0;
+
+            Minimum = array[0];
+
+            for(int j = 0; j < array.Length; j++)
+            {
+                if (Minimum > array[j])
+                    Minimum = array[j];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Minimum = {0}", Minimum);
+
+            for(int k = 0; k < array.Length; k++)
+            {
+                if (Maximum < array[k])
+                    Maximum = array[k];
+            }
+
+            Console.WriteLine("Maximum = {0}\n", Maximum);
+
+            Console.ReadKey();
+        }
+    }
+}
