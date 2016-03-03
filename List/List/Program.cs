aee30839b13f01f5302d1aa48033749d6a154490
Vadym <Vadymshmorgun23@gmail.com>
2016-03-03 12:22:47 +0200

    add

---
+++ List/List/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+            List<string> sandwich = new List<string>();
+
+            sandwich.Add("bacon");
+            sandwich.Add("tomato");
+
+            sandwich.Insert(1, "lettuce");
+
+            foreach (string ingredient in sandwich)
+            {
+                System.Console.WriteLine(ingredient);
+            }
+            Console.ReadLine();
+        }
+    }
+}
