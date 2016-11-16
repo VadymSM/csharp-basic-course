c34588706a422d03f7d7c66137399dcb3b982f24
michael-trusov <trusov.michael@gmail.com>
2016-11-16 22:29:01 +0200

    Tasks for Order

--- McDonalds/McDonalds.BL/Order.cs
+++ McDonalds/McDonalds.BL/Order.cs
@@ -8,5 +8,20 @@ namespace McDonalds.BL
 {
     class Order
     {
+        // [T] list of products connected with the Order
+        // [T] list of prepared products
+
+        // [T] AddProduct method
+        // [T] RemoveProduct method
+
+        // [T] declare delegate for event
+        // [T] event Ready, occur when all products in Order are prepared.
+        //      when prepared list is equal products list we should generate event
+
+        // [T] ProductWasPrepared(Product)  - add product to the prepared list
+
+        // [T] Property Price - dynamicly calculate price of the Order
+
+        // [T]!!!! cover class by unit tests!
     }
 }
